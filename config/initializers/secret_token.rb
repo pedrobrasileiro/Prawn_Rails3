a090463e84599262f32d0ff914f7d0d41af18ca0
Pedro Brasileiro <pedrobrasileiro@gmail.com>
2010-10-19 14:55:22 -0300

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestePrawn::Application.config.secret_token = 'daef6c780d32b43bd6865319159b4cafa640ff246bfd1fd535ec77bab2c9c72248efb4adf1485c5f51df949c9c06d725e509d577ef9d4407472cac60cf744008'
